3fa8c39236072a6ea6c9ec6ecebf262686023e86
matheus <matheus.bsilva@bandtec.com.br>
2020-09-14 23:52:10 -0300

    Login e logoff da aplicação

---
+++ back-end/urent/src/main/java/com/estrutura/urent/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.estrutura.urent.controller;
+
+import com.estrutura.urent.model.Locador;
+import com.estrutura.urent.model.Locatario;
+import com.estrutura.urent.model.Login;
+import com.estrutura.urent.model.Usuario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class LoginController {
+
+    private static boolean login = false;
+
+    Locador locador = new Locador("Matheus", "32659785625", "matheus.custodio@gmail.com", "123", "05648526", 5);
+    Locatario locatario = new Locatario("Vinicius", "65945826648", "vinivius.sobral@gmail.com", "456", "02645236", 5, "5648595");
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody Login login) {
+        if(login.getEmail().equals(locador.getEmail()) && login.getSenha().equals(locador.getSenha())) {
+            this.login = true;
+            return ResponseEntity.ok(this.locador);
+        } else if(login.getEmail().equals(locatario.getEmail()) && login.getSenha().equals(locatario.getSenha())) {
+            this.login = true;
+            return ResponseEntity.ok(this.locatario);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/logoff")
+    public ResponseEntity logoff() {
+        if(this.login) {
+            this.login = false;
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    public static boolean isLogin() {
+        return login;
+    }
+}
