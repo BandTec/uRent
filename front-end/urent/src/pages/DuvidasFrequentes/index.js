6969968b0ef10d96ff9754f631c6f28c4a37d0d4
viniciussobral <vinicius.araujo@bandtec.com.br>
2020-10-02 12:55:29 -0300

    Duvidas Frequentes Atualizado-2

--- front-end/urent/src/pages/DuvidasFrequentes/index.js
+++ front-end/urent/src/pages/DuvidasFrequentes/index.js
@@ -13,10 +13,26 @@ function Duvidas() {
 
             <HeaderDuvidas />
 
-            <S.Welcome>
+            <div>
+
+                <S.Welcome>
+
+                    <S.FormPerguntas>
+                        <S.FormTitle>Como cancelo um anuncio?</S.FormTitle>
+                        <S.FormTitle>Reservei uma garagem chegando lá estava ocupada , o que faço?</S.FormTitle>
+                        <S.FormTitle>O pagamento e feito pelo site ou pessoalmente?</S.FormTitle>
+                        <S.FormTitle>O que é a Urent e como funciona?</S.FormTitle>
+                        <S.FormTitle>Como faço pra efetuar uma reserva?</S.FormTitle>
+                        <S.FormTitle>Como anunciar uma garagem?</S.FormTitle>
+                        <S.FormTitle>Como altero meu endereço?</S.FormTitle>
 
 
-            </S.Welcome>
+                    </S.FormPerguntas>
+
+                </S.Welcome>
+
+            </div>
+
 
             <Footer />
         </div>
--- front-end/urent/src/pages/DuvidasFrequentes/style.js
+++ front-end/urent/src/pages/DuvidasFrequentes/style.js
@@ -1,10 +1,35 @@
 import style from 'styled-components';
 
 export const Welcome = style.div`
-    height: 300px;
+    height: 380px;
     background-color: #E4EEFC;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
+`;
+
+
+export const FormPerguntas = style.div`
+
+    margin-bottom: 220px;
+    height: 200px;
+    width: 50em;
+    justify-content: center;
+    position: absolute;
+    align-items: center;
+
+
+`;
+
+export const FormTitle = style.p`
+    height: 30px;
+    margin-bottom: 25px;
+    background-color: white;
+    margin: 10px;
+    padding-top: 5px;
+    border: 1px solid #6A6363;
+    border-radius: 4px;
+    text-align: center;
+
 `;
