437418c993cd46af1d6bc2db058ea37e91e781f0
matheus <matheus.bsilva@bandtec.com.br>
2020-10-04 13:05:05 -0300

    checkbox alinhado

--- front-end/urent/src/pages/Login/index.js
+++ front-end/urent/src/pages/Login/index.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import {FaUser,FaKey} from 'react-icons/fa';
+import { FaUser, FaKey } from 'react-icons/fa';
 
 
 import logo from '../../assets/logoLogin.svg';
 import * as S from '../Login/style';
 
-function Login(){
+function Login() {
     return (
         <div>
-           <S.Welcome>
-           
-           <S.FormLogin>
+            <S.Welcome>
 
-            <S.LogoImage><img src={logo} alt="Logo uRent" height="100%"/></S.LogoImage>
-            <S.Form>
-            <S.FormTitleEntrar>Entrar</S.FormTitleEntrar>
-            <S.FormTitleLabel >E-mail</S.FormTitleLabel>
-            <S.FormInput placeholder="ex@gmail.com"/>
-            <S.FormTitleLabel>Senha</S.FormTitleLabel>
-            <S.FormInput placeholder="*************"/>
-            <S.FormRecursos>
-                <S.InputCheck type="checkbox"/>
-                <S.LabelCheck>Lembrar-me</S.LabelCheck>
-                <S.LabelCheck>Esqueci minha senha</S.LabelCheck>
-            </S.FormRecursos>
-            <S.ButtonEntrar>ENTRAR</S.ButtonEntrar>
-            <S.LabelCheck>Criar conta</S.LabelCheck>
-            </S.Form>
-            
-            </S.FormLogin>
-        
-           </S.Welcome>
+                <S.FormLogin>
+
+                    <S.LogoImage><img src={logo} alt="Logo uRent" height="100%" /></S.LogoImage>
+                    <S.Form>
+                        <S.FormTitleEntrar>Entrar</S.FormTitleEntrar>
+                        <S.FormTitleLabel >E-mail</S.FormTitleLabel>
+                        <S.FormInput placeholder="ex@gmail.com" />
+                        <S.FormTitleLabel>Senha</S.FormTitleLabel>
+                        <S.FormInput placeholder="*************" />
+                        <S.FormRecursos>
+                            <S.FormRecusosCheck style={{display: 'flex', alignItems: 'center'}}>
+                                <input type="checkbox" type="checkbox" />
+                                <S.LabelCheck>Lembrar-me</S.LabelCheck>
+                            </S.FormRecusosCheck>
+                            <S.LabelCheck>Esqueci minha senha</S.LabelCheck>
+                        </S.FormRecursos>
+                        <S.ButtonEntrar>ENTRAR</S.ButtonEntrar>
+                        <S.LabelCheck>Criar conta</S.LabelCheck>
+                    </S.Form>
+
+                </S.FormLogin>
+
+            </S.Welcome>
         </div>
     )
 }
--- front-end/urent/src/pages/Login/style.js
+++ front-end/urent/src/pages/Login/style.js
@@ -1,69 +1,21 @@
 import style from 'styled-components';
 
 export const Welcome = style.div`
-width: 100%;
-height:100vh;
-position:absolute;
-background: #E4EEFC;
+    width: 100%;
+    height:100vh;
+    position:absolute;
+    background: #E4EEFC;
 `;
 
 export const FormLogin = style.div`
-width: 80%;
-height: 540px;
-margin: 50px auto;
-background-color:#2466E2;
-border-radius: 8px;
-display:flex;
-justify-content:space-around;
-align-items: center;
-`;
-
-export const FormTitleEntrar = style.h1`
-
-
-
-color: #FFFFFF;
-
-`;
-
-export const FormTitleLabel=style.label`
-align-self:flex-start;
-color:#FFFFFF;
-margin-bottom:5px;
-`;
-
-export const FormInput = style.input`
-width: 445px;
-height: 50px;
-padding-left:15px;
-background-color:#E4EEFC;
-border-radius: 8px;
-border: 1px solid #E4EEFC;
-margin-bottom:15px;
-
-`;
-
-
-export const Form= style.div` 
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items: center;
-`;
-
-export const FormRecursos= style.div`
-margin-bottom:30px;
-display:flex;
-justify-content:space-between;
-width:100%;
-`;
-
-export const InputCheck=style.input`
-
-`;
-
-export const LabelCheck= style.label`
-color:white;
+    width: 80%;
+    height: 540px;
+    margin: 50px auto;
+    background-color:#2466E2;
+    border-radius: 8px;
+    display:flex;
+    justify-content:space-around;
+    align-items: center;
 `;
 
 export const LogoImage = style.div`
@@ -71,19 +23,60 @@ export const LogoImage = style.div`
     margin-left:50px;
 `;
 
-export const ButtonEntrar = style.button`
-&:hover{
-    background: rgba(20, 31, 44, 0.69);
-    border: 1px solid rgba(20, 31, 44, 0.69);
-}
-font-size:23px;
-font-weight:700;
-color:#FFFFFF;
-width: 100%;
-height: 63px;
-margin-bottom:20px;
-border-radius: 8px;
-background: rgba(11, 21, 34, 0.49);
-border: 1px solid rgba(11, 21, 34, 0.49);
+export const Form= style.div` 
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items: center;
+`;
 
+export const FormTitleEntrar = style.h1`
+    color: #FFFFFF;
+`;
+
+export const FormTitleLabel = style.label`
+    align-self:flex-start;
+    color:#FFFFFF;
+    margin-bottom:5px;
+`;
+
+export const FormInput = style.input`
+    width: 445px;
+    height: 50px;
+    padding-left:15px;
+    background-color:#E4EEFC;
+    border-radius: 8px;
+    border: 1px solid #E4EEFC;
+    margin-bottom:15px;
+`;
+
+export const FormRecursos= style.div`
+    margin-bottom:30px;
+    display:flex;
+    justify-content:space-between;
+    width:100%;
+`;
+
+export const FormRecusosCheck = style.div`
+    display: flex;
+    align-items: center;
+`
+
+export const LabelCheck= style.label`
+    color:white;
+`;
+export const ButtonEntrar = style.button`
+    font-size: 23px;
+    font-weight: 700;
+    color: #FFFFFF;
+    width: 100%;
+    height: 63px;
+    margin-bottom:20px;
+    border-radius: 8px;
+    background-color: rgba(11, 21, 34, 0.49);
+    border: 1px solid rgba(11, 21, 34, 0.49);
+    &:hover{
+        background: rgba(20, 31, 44, 0.69);
+        border: 1px solid rgba(20, 31, 44, 0.69);
+    }
 `;
