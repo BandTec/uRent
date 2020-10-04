7a94cbed04cd730516baf5a4bb0e975b9a9b35d8
matheus <matheus.bsilva@bandtec.com.br>
2020-10-04 20:46:19 -0300

    Tela de alugar

---
+++ front-end/urent/src/pages/Alugar/style.js
@@ -0,0 +1,14 @@
+import style from 'styled-components';
+
+export const Section = style.div`
+    // background-color: red;
+    width: 80%;
+    height: 90vh;
+    margin: 0 auto 90px auto;
+`;
+
+export const SectionTitle = style.h1`
+    color: #9C98A6;
+    font-size: 20px;
+`;
+
