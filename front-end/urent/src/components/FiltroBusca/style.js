60ad3e68f812d79dec467f9db378482ccca8bd1b
matheus <matheus.bsilva@bandtec.com.br>
2020-10-07 17:07:25 -0300

    Configurando o filtro para quando clicar abrir

--- front-end/urent/src/components/FiltroBusca/style.js
+++ front-end/urent/src/components/FiltroBusca/style.js
@@ -6,16 +6,15 @@ export const Fitlro = style.div`
     height: 75px;
     border: 1px solid #C4C4C4;
     border-radius: 15px;
-    margin: 0 auto 60px auto;
+    margin: 0 auto 0 auto;
     padding: 0 40px;
     box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.65);
     display: flex;
     justify-content: space-between;
-    align-items: center;
 `;
 
 export const FiltroSection = style.div`
-    // background-color: green;
+    // background-color: yellow;
     width: 30%;
     height: 100%;
     display: flex;
@@ -30,4 +29,37 @@ export const FiltroSectionTitle = style.div`
 
 export const FiltroSectionText = style.div`
     font-size: 20px;
+`;
+
+export const FiltroOpen = style.div`
+    // background-color: green; 
+    width: 80%;
+    height: 250px;
+    border: 1px solid #C4C4C4;
+    border-radius: 15px;
+    margin: 0 auto 60px auto;
+    padding: 0 40px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const FiltroLabelOpen = style.label`
+    display: block;
+    margin: 15px 0 5px 0;
+`;
+
+export const FilterInputOpen = style.input`
+    width: 80%;
+    height: 30px;
+    border: 1px solid #E6E6F0;
+    border-radius: 8px;
+    padding-left: 15px;
+`;
+
+export const FilterTipoVeiculo = style.label`
+    // background-color: red;
+    font-size: 18px;
+    height: 30px;
+    display: flex;
+    align-items: center;
 `;
