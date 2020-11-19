02da3a2ea564e3b8197b38253e59562b4223a178
matheus <matheus.bsilva@bandtec.com.br>
2020-11-19 17:04:59 -0300

    Novos métodos na repository

--- back-end/urent/src/main/java/com/projeto/urent/repositorios/AnuncioRepository.java
+++ back-end/urent/src/main/java/com/projeto/urent/repositorios/AnuncioRepository.java
@@ -1,6 +1,8 @@
 package com.projeto.urent.repositorios;
 
 import com.projeto.urent.dominios.Anuncio;
+import com.projeto.urent.visoes.AnuncioSimplesAluguel;
+import com.projeto.urent.visoes.AnuncioSimplesFeed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,10 @@ public interface AnuncioRepository extends JpaRepository<Anuncio, Integer> {
 
     @Query("select a from Anuncio a join Garagem g on a.garagem = g.id join Usuario u on g.usuario = u.id where u.id = ?1")
     List<Anuncio> findAllByUsuario(Integer id);
+
+    @Query("select new com.projeto.urent.visoes.AnuncioSimplesAluguel(a.valorDiaria, g.latitude, g.longitude) from Anuncio a join Garagemg g on a.garagem = g.id")
+    List<AnuncioSimplesAluguel> findLatLong();
+
+    @Query("select new com.projeto.urent.visoes.AnuncioSimplesFeed(a.id, a.titulo, a.valorDiaria, t.tipo, g.cep, g.numero, g.avaliacao) from Anuncio a join Garagem g on a.garagem = g.id join TipoVeiculo t on a.tipoVeiculo = t.id")
+    List<AnuncioSimplesFeed> findFeed();
 }
