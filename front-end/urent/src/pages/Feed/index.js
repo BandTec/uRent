22ed2d0bfde0c9dcfd7f073070ceb6c3e80b9a53
matheus <matheus.bsilva@bandtec.com.br>
2020-11-19 17:06:16 -0300

    Feed em desenvolvimento

--- front-end/urent/src/pages/Feed/index.js
+++ front-end/urent/src/pages/Feed/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoIosStar } from 'react-icons/io';
 
 import HeaderAnuncio from '../../components/HeaderAnuncio/index';
 import FiltroBusca from '../../components/FiltroBusca/index';
 import Footer from '../../components/Footer';
+
+import api from '../../service/api';
 
 import foto from '../../assets/garagemExemplo.png';
 
@@ -11,53 +13,101 @@ import * as S from './style';
 
 function Feed() {
 
-	return (
-		<div>
-			<HeaderAnuncio />
-			<FiltroBusca />
+  const [anuncios, setAnuncios] = useState([]);
+  const [endereco, setEndereco] = useState(['rua']);
+  let logradouros = [];
 
-			<S.FeedContainer>
+  // logradouros.push('Rua Zeze')
 
-				<S.FeedTitle>Garagem disponível para aluguel diário</S.FeedTitle>
-				<S.FeedSection>
+  useEffect(() => {
+    api.get('/anuncios/feed')
+      .then((response) => {
+        // console.log(response.data)
+        setAnuncios(response.data);
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
-					<div>
-						<img src={foto} height="250px" alt="Foto do anúncio" />
-						<S.FeedSection>
-							<label style={{fontWeight: '500'}}>6.0</label>
-							<div>
-								<IoIosStar size="20" color="#FDF53B" />
-								<IoIosStar size="20" color="#FDF53B" />
-								<IoIosStar size="20" color="#FDF53B" />
-								<IoIosStar size="20" color="#C4C4C4" />
-								<IoIosStar size="20" color="#C4C4C4" />
-							</div>
-						</S.FeedSection>
-					</div>
-					<S.FeedContent>
+  useEffect(() => {
+    for(let i = 0; i < anuncios.length; i++) {
+      api.get(`https://viacep.com.br/ws/${anuncios[i].cep}/json`)
+      .then(response => {
+        logradouros.push({
+          "rua": response.data.logradouro
+        })
+      })
+    }
+    setEndereco({logradouros: logradouros});
+    // endereco.logradouros.map(logradouro =>
+    //   console.log(logradouro + " teste")
+    // )
+  }, [anuncios])
 
-						<S.FeedContentTitle>Localização</S.FeedContentTitle>
+  console.log(endereco);
 
-						<S.FeedContentSection>
-						
-							<S.SectionTitle>Endereço: <S.SectionData>Endereço do anúncio</S.SectionData></S.SectionTitle>
-							<S.SectionTitle>Nº: <S.SectionData>234</S.SectionData></S.SectionTitle> <S.SectionTitle>CEP: <S.SectionData>45896125-648</S.SectionData></S.SectionTitle>
-							<S.SectionTitle>Cidade: <S.SectionData>São Paulo</S.SectionData></S.SectionTitle>
-						
-						</S.FeedContentSection>
-						<S.SectionTitle>Tipo de Garagem: <S.SectionData>Carros</S.SectionData></S.SectionTitle>
-						<S.SectionPreco style={{alignSelf: 'center'}}>Preço diário: <S.SectionData>R$15,00</S.SectionData></S.SectionPreco>
-						
-					</S.FeedContent>
-				
-				</S.FeedSection>
+  return (
+    <div>
+      <HeaderAnuncio />
+      <FiltroBusca />
 
-			</S.FeedContainer>
+      <div>
+        {
+          anuncios.map(anuncio =>
+            <S.FeedContainer key={anuncio.id}>
+              <S.FeedTitle>{anuncio.titulo}</S.FeedTitle>
+              <S.FeedSection>
 
-			<Footer />
+                <div>
+                  <img src={foto} height="250px" alt="Foto do anúncio" />
+                  <S.FeedSection>
+                    <label style={{ fontWeight: '500' }}>6.0</label>
+                    <div>
+                      <IoIosStar size="20" color="#FDF53B" />
+                      <IoIosStar size="20" color="#FDF53B" />
+                      <IoIosStar size="20" color="#FDF53B" />
+                      <IoIosStar size="20" color="#C4C4C4" />
+                      <IoIosStar size="20" color="#C4C4C4" />
+                    </div>
+                  </S.FeedSection>
+                </div>
+                <S.FeedContent>
 
-		</div>
-	);
+                  <S.FeedContentTitle>Localização</S.FeedContentTitle>
+
+                  <S.FeedContentSection>
+
+                    <S.SectionTitle>Endereço:
+											<S.SectionData>
+                        {
+                          endereco.logradouros.map(logradouro =>
+                          console.log(logradouro + " teste")
+                          )}
+                      </S.SectionData>
+                    </S.SectionTitle>
+                    <S.SectionTitle>Nº: <S.SectionData>{anuncio.numero}</S.SectionData></S.SectionTitle> <S.SectionTitle>CEP: <S.SectionData>{anuncio.cep}</S.SectionData></S.SectionTitle>
+                    <S.SectionTitle>Cidade: <S.SectionData>{ }</S.SectionData></S.SectionTitle>
+
+                  </S.FeedContentSection>
+                  <S.SectionTitle>Tipo de Garagem: <S.SectionData>{anuncio.tipo}</S.SectionData></S.SectionTitle>
+                  <S.SectionPreco style={{ alignSelf: 'center' }}>Preço diário: <S.SectionData>{anuncio.valorDiaria}</S.SectionData></S.SectionPreco>
+
+                </S.FeedContent>
+
+
+              </S.FeedSection>
+            </S.FeedContainer>
+          )
+        }
+
+
+      </div>
+
+      <Footer />
+
+    </div>
+  );
 }
 
 export default Feed;
