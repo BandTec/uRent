61336fc45a50d4135390401404ab7b35204dd26a
matheus <matheus.bsilva@bandtec.com.br>
2020-12-01 21:23:51 -0300

    Adaptações na service

--- back-end/urent/src/main/java/com/projeto/urent/servicos/FileStorageService.java
+++ back-end/urent/src/main/java/com/projeto/urent/servicos/FileStorageService.java
@@ -3,12 +3,15 @@ package com.projeto.urent.servicos;
 import com.projeto.urent.dominios.FileDB;
 import com.projeto.urent.dominios.Garagem;
 import com.projeto.urent.repositorios.FileDBRepository;
+import com.projeto.urent.repositorios.GaragemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 @Service
@@ -18,9 +21,10 @@ public class FileStorageService {
     @Autowired
     private FileDBRepository fileDBRepository;
 
-    public FileDB store(MultipartFile file) throws IOException {
+    public FileDB store(MultipartFile file, Integer id) throws IOException {
+
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes(), id);
 
         return fileDBRepository.save(FileDB);
     }
@@ -29,7 +33,7 @@ public class FileStorageService {
         return fileDBRepository.findById(id).get();
     }
 
-    public Stream<FileDB> getAllFiles() {
-        return fileDBRepository.findAll().stream();
-    }
+//    public List<FileDB> getAllFiles() {
+//        return fileDBRepository.findAll();
+//    }
 }
