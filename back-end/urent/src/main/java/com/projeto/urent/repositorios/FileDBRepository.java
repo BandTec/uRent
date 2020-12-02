d14cb6d9b9cc7eebc4c50ac3242fd59e50a0d7f2
matheus <matheus.bsilva@bandtec.com.br>
2020-12-01 21:23:40 -0300

    Método da interface para buscar arquivo pelo id da garagem

--- back-end/urent/src/main/java/com/projeto/urent/repositorios/FileDBRepository.java
+++ back-end/urent/src/main/java/com/projeto/urent/repositorios/FileDBRepository.java
@@ -2,10 +2,14 @@ package com.projeto.urent.repositorios;
 
 import com.projeto.urent.dominios.FileDB;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface FileDBRepository extends JpaRepository<FileDB, String> {
 
+    FileDB findAllByGaragem(Integer id);
 }
