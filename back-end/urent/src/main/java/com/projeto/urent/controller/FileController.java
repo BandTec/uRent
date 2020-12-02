be63eb924af7f0387db7ddcc483f07b981d63241
matheus <matheus.bsilva@bandtec.com.br>
2020-12-01 21:22:35 -0300

    Upload da imagem da garagem funcionando

--- back-end/urent/src/main/java/com/projeto/urent/controller/FileController.java
+++ back-end/urent/src/main/java/com/projeto/urent/controller/FileController.java
@@ -3,6 +3,7 @@ package com.projeto.urent.controller;
 import com.projeto.urent.dominios.FileDB;
 import com.projeto.urent.message.ResponseFile;
 import com.projeto.urent.message.ResponseMessage;
+import com.projeto.urent.repositorios.FileDBRepository;
 import com.projeto.urent.servicos.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -13,24 +14,30 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
-@Controller
-@CrossOrigin("http://localhost:8081")
+@RestController
+@RequestMapping("/imagens")
+@CrossOrigin
 public class FileController {
-
 
     @Autowired
     private FileStorageService storageService;
 
+    @Autowired
+    private FileDBRepository repository;
+
     @PostMapping("/upload")
-    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("idGaragem") Integer id) {
+        System.out.println(id);
+        System.out.println(file.getName());
         String message = "";
         try {
-            storageService.store(file);
-
+            storageService.store(file, id);
             message = "Uploaded the file successfully: " + file.getOriginalFilename();
             return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
         } catch (Exception e) {
@@ -41,21 +48,49 @@ public class FileController {
 
     @GetMapping("/files")
     public ResponseEntity<List<ResponseFile>> getListFiles() {
-        List<ResponseFile> files = storageService.getAllFiles().map(dbFile -> {
+
+        List<FileDB> files = repository.findAll();
+        List<ResponseFile> filesReturn = new ArrayList<>();
+
+        System.out.println(files);
+
+        for(int i = 0; i < files.size(); i++) {
+            System.out.println("id: "+ files.get(i).getId());
             String fileDownloadUri = ServletUriComponentsBuilder
                     .fromCurrentContextPath()
-                    .path("/files/")
-                    .path(dbFile.getId())
+                    .path("/imagens/files/")
+                    .path(files.get(i).getId())
                     .toUriString();
 
-            return new ResponseFile(
-                    dbFile.getName(),
+            ResponseFile responseFile = new ResponseFile(
+                    files.get(i).getName(),
                     fileDownloadUri,
-                    dbFile.getType(),
-                    dbFile.getData().length);
-        }).collect(Collectors.toList());
+                    files.get(i).getType(),
+                    files.get(i).getData().length);
 
-        return ResponseEntity.status(HttpStatus.OK).body(files);
+            filesReturn.add(responseFile);
+
+        }
+        return ResponseEntity.ok(filesReturn);
+    }
+
+    @GetMapping("/files/garagem/{idGaragem}")
+    public ResponseEntity getFilesGaragem(@PathVariable Integer idGaragem) {
+        FileDB fileDB = repository.findAllByGaragem(idGaragem);
+
+        String fileDowloadUri = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/imagens/files/")
+                .path(fileDB.getId())
+                .toUriString();
+
+        ResponseFile responseFile = new ResponseFile(
+                fileDB.getName(),
+                fileDowloadUri,
+                fileDB.getType(),
+                fileDB.getData().length);
+
+        return ResponseEntity.ok(responseFile);
     }
 
     @GetMapping("/files/{id}")
